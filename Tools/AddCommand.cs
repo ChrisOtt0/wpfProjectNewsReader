df7fc2efcbcee1c2e1f8f2e2ecefbe4a8d9fcd4e
ChrisOtt0 <sophusott@gmail.com>
2022-10-31 12:07:04 +0100

    Init commit

---
+++ Tools/AddCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace wpfProjectNewsReader.Tools
+{
+    public class AddCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+        private Action<Object>? actionToInvoke = null;
+
+        public bool CanExecute(object? parameter)
+        {
+            if (actionToInvoke != null)
+                return true;
+            else
+                return false;
+        }
+
+        public void Execute(object? parameter)
+        {
+            if (actionToInvoke != null)
+                this.actionToInvoke(parameter);
+        }
+
+        public AddCommand(Action<Object> actionToInvoke)
+        {
+            this.actionToInvoke = actionToInvoke;
+        }
+    }
+}
